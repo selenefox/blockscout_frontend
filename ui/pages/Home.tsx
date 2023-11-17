00cd3073a5aaac91d7558201b391437d39729b16
devopt <devopt@bitplanet.global>
2023-11-17 07:39:49 +0000

    fix the text error

--- ui/pages/Home.tsx
+++ ui/pages/Home.tsx
@@ -30,7 +30,7 @@ const Home = () => {
             fontWeight={ 600 }
             color={ config.UI.homepage.plate.textColor }
           >
-            Welcome to { config.chain.name } explorer
+            Welcome to { config.chain.name } Explorer
           </Heading>
           <Box display={{ base: 'none', lg: 'block' }}>
             { config.features.account.isEnabled && <ProfileMenuDesktop/> }
